f019056994667e07f7fde51c68270f7869a7a5f8
Admin <admin.push@hackerrank.workspace>
2022-04-25 11:40:49 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // Js code goes here
+var id=document.getElementById("current-question-id").value;
+var link= `https://jsonmock.hackerrank.com/api/questions/${id}`;
+
+ var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   document.getElementById("loader-view").style.display="none";
+    console.log(this.responseText);
+  }
+};
+xhttp.open("GET", link, true);
+xhttp.send();
